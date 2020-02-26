1134819ca2733cc888476bbbeba89e254838de8a
Yazdani <yaz4dani@gmail.com>
2020-02-26 23:16:39 +0600

    Implement Firestore

--- lib/london/London_Eat.dart
+++ lib/london/London_Eat.dart
@@ -78,127 +78,182 @@ class _LondonEatState extends State<LondonEat> {
                                           ),
                                         ),
                                         Positioned(
-                                          top: 80.0,
+                                          top: 10.0,
                                           left: 10.0,
                                           child: Container(
-                                            height: 150.0,
-                                            width: 340.0,
-                                            child: Card(
-                                              elevation: 10.0,
-                                              shape: RoundedRectangleBorder(
-                                                  borderRadius: BorderRadius
-                                                      .circular(20.0)
-                                              ),
-                                              child: Column(
-                                                crossAxisAlignment: CrossAxisAlignment
-                                                    .start,
-                                                children: <Widget>[
-                                                  Container(
-                                                    child: Row(
-                                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                
+                                                Container(
+                                                  height: 40.0,
+                                                  width: 100.0,
+                                                  decoration: BoxDecoration(
+                                                    borderRadius: BorderRadius.circular(10.0),
+                                                    color: Color(0xFF37a000),
+                                                  ),
+                                                  child: Align(
+                                                    alignment: Alignment.center,
+                                                    child: Text("Offer",
+                                                    style: TextStyle(
+                                                      fontSize: 20.0,
+                                                      color: Colors.white
+                                                    ),
+                                                    ),
+                                                  ),
+                                                ),
+                                                SizedBox(height: 40.0,),
+                                                Container(
+                                                  height: 150.0,
+                                                  width: 340.0,
+                                                  child: Card(
+                                                    elevation: 10.0,
+                                                    shape: RoundedRectangleBorder(
+                                                        borderRadius: BorderRadius
+                                                            .circular(20.0)
+                                                    ),
+                                                    child: Column(
+                                                      crossAxisAlignment: CrossAxisAlignment
+                                                          .start,
                                                       children: <Widget>[
 
                                                         Container(
-                                                          padding: EdgeInsets.all(
+                                                          child: Row(
+                                                            mainAxisAlignment: MainAxisAlignment
+                                                                .spaceBetween,
+                                                            children: <Widget>[
+
+                                                              Container(
+                                                                padding: EdgeInsets
+                                                                    .all(
+                                                                    10.0),
+                                                                child: Text(
+                                                                  ourDate
+                                                                      .data['title'],
+                                                                  maxLines: 1,
+                                                                  style: TextStyle(
+                                                                      fontSize: 18.0,
+                                                                      fontWeight: FontWeight
+                                                                          .bold
+                                                                  ),
+                                                                ),
+                                                              ),
+                                                              Container(
+                                                                child: Icon(
+                                                                  Icons
+                                                                      .arrow_forward,
+                                                                  size: 30.0,
+                                                                  color: Colors
+                                                                      .amber,),
+                                                                margin: EdgeInsets
+                                                                    .only(
+                                                                    right: 15.0),
+                                                              )
+
+                                                            ],
+                                                          ),
+                                                        ),
+                                                        
+                                                        SizedBox(height: 6.0,),
+                                                        Container(
+                                                          padding: EdgeInsets
+                                                              .all(
                                                               10.0),
-                                                          child: Text(
-                                                            ourDate.data['title'],
+                                                          child: Text("\$" +
+                                                              ourDate
+                                                                  .data['price'],
                                                             maxLines: 1,
                                                             style: TextStyle(
                                                                 fontSize: 18.0,
                                                                 fontWeight: FontWeight
-                                                                    .bold
+                                                                    .bold,
+                                                                color: Colors
+                                                                    .deepOrange
                                                             ),
                                                           ),
                                                         ),
 
+                                                        SizedBox(height: 5.0,),
+
                                                         Container(
-                                                            child: Icon(Icons.arrow_forward,size: 30.0,color: Colors.amber,),
-                                                          margin: EdgeInsets.only(right: 15.0),
+                                                          margin: EdgeInsets
+                                                              .all(10.0),
+                                                          child: Row(
+                                                            children: <Widget>[
+
+                                                              Expanded(
+                                                                flex: 2,
+                                                                child: Row(
+                                                                  children: <
+                                                                      Widget>[
+                                                                    Icon(Icons
+                                                                        .fastfood),
+                                                                    SizedBox(
+                                                                      width: 5.0,),
+                                                                    Text(
+                                                                      "Dessert",
+                                                                      style: TextStyle(
+                                                                        fontSize: 20.0,
+                                                                      ),
+                                                                    )
+                                                                  ],
+                                                                ),
+                                                              ),
+                                                              Expanded(
+                                                                flex: 1,
+                                                                child: Row(
+                                                                  children: <
+                                                                      Widget>[
+                                                                    Icon(Icons
+                                                                        .location_city),
+                                                                    SizedBox(
+                                                                      width: 5.0,),
+                                                                    Text(ourDate
+                                                                        .data['distance'] +
+                                                                        "km",
+                                                                      style: TextStyle(
+                                                                        fontSize: 20.0,
+                                                                      ),
+                                                                    )
+                                                                  ],
+                                                                ),
+                                                              ),
+
+                                                              Expanded(
+                                                                flex: 1,
+                                                                child: Row(
+                                                                  children: <
+                                                                      Widget>[
+                                                                    Icon(Icons
+                                                                        .person_outline),
+                                                                    SizedBox(
+                                                                      width: 5.0,),
+                                                                    Text(ourDate
+                                                                        .data['person'],
+                                                                      style: TextStyle(
+                                                                        fontSize: 20.0,
+                                                                      ),
+                                                                    )
+                                                                  ],
+                                                                ),
+                                                              ),
+
+
+                                                            ],
+                                                          ),
                                                         )
 
-                                                      ],
-                                                    ),
-                                                  ),
-                                                  SizedBox(height: 6.0,),
-                                                  Container(
-                                                    padding: EdgeInsets.all(
-                                                        10.0),
-                                                    child: Text("\$" +
-                                                        ourDate.data['price'],
-                                                      maxLines: 1,
-                                                      style: TextStyle(
-                                                          fontSize: 18.0,
-                                                          fontWeight: FontWeight
-                                                              .bold,
-                                                          color: Colors
-                                                              .deepOrange
-                                                      ),
-                                                    ),
-                                                  ),
-
-                                                  SizedBox(height: 5.0,),
-
-                                                  Container(
-                                                    margin: EdgeInsets.all(10.0),
-                                                    child: Row(
-                                                      children: <Widget>[
-
-                                                        Expanded(
-                                                          flex: 2,
-                                                          child: Row(
-                                                            children: <Widget>[
-                                                              Icon(Icons.fastfood),
-                                                              SizedBox(width: 5.0,),
-                                                              Text("Dessert",
-                                                              style: TextStyle(
-                                                                fontSize: 20.0,
-                                                              ),
-                                                              )
-                                                            ],
-                                                          ),
-                                                        ),
-                                                        Expanded(
-                                                          flex: 1,
-                                                          child: Row(
-                                                            children: <Widget>[
-                                                              Icon(Icons.location_city),
-                                                              SizedBox(width: 5.0,),
-                                                              Text(ourDate.data['distance']+"km",
-                                                                style: TextStyle(
-                                                                  fontSize: 20.0,
-                                                                ),
-                                                              )
-                                                            ],
-                                                          ),
-                                                        ),
-
-                                                        Expanded(
-                                                          flex: 1,
-                                                          child: Row(
-                                                            children: <Widget>[
-                                                              Icon(Icons.person_outline),
-                                                              SizedBox(width: 5.0,),
-                                                              Text(ourDate.data['person'],
-                                                                style: TextStyle(
-                                                                  fontSize: 20.0,
-                                                                ),
-                                                              )
-                                                            ],
-                                                          ),
-                                                        ),
-
-
 
                                                       ],
                                                     ),
-                                                  )
+                                                  ),
+                                                ),
 
 
-
-                                                ],
-                                              ),
+                                              ],
                                             ),
+
                                           ),
                                         )
 
