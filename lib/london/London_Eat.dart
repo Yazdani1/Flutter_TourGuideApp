b2f01d5b6f18b646bc23a2a388e04e14a2ee2f58
Yazdani <yaz4dani@gmail.com>
2020-02-27 14:17:19 +0600

    Implement Firestore

--- lib/london/London_Eat.dart
+++ lib/london/London_Eat.dart
@@ -342,11 +342,32 @@ class _LondonEatState extends State<LondonEat> {
                 child: ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (BuildContext context,index){
+                    var ourData = snapshot.data[index];
                     return Container(
                       height: 250.0,
                       child: Row(
                         children: <Widget>[
-                          
+
+                          Expanded(
+                            flex: 1,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: Image.network(ourData.data['img'],
+                              height: 250.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 5.0,),
+                          Expanded(
+                            flex: 2,
+                            child: Column(
+                              children: <Widget>[
+                                
+                              ],
+                            ),
+                          )
+
                         ],
                       ),
                     );
