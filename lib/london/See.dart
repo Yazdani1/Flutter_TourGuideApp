be8ea335678f1c864efa9575db72656bc84f216f
Yazdani <yaz4dani@gmail.com>
2020-03-03 14:03:09 +0600

    Implement Firestore

--- lib/london/See.dart
+++ lib/london/See.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class See extends StatefulWidget {
   @override
@@ -6,10 +7,52 @@ class See extends StatefulWidget {
 }
 
 class _SeeState extends State<See> {
+
+  Future getPlaces()async {
+    var firestore = Firestore.instance;
+    QuerySnapshot snapshot = await firestore.collection("seePlaces").getDocuments();
+    return snapshot.documents;
+  }
+  Future getRefresh()async{
+    await Future.delayed(Duration(seconds: 3));
+    setState(() {
+      getPlaces();
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       
+      appBar: AppBar(
+        title: Text("Tourist Places"),
+        backgroundColor: Colors.amber,
+      ),
+
+      body: FutureBuilder(
+         future: getPlaces(),
+        builder: (BuildContext context,snapshot){
+           if(snapshot.connectionState == ConnectionState.waiting){
+             return Center(
+               child: CircularProgressIndicator(),
+             );
+           }else{
+             return RefreshIndicator(
+               onRefresh: getRefresh,
+               child: GridView.builder(
+                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                   itemBuilder: (BuildContext c,index){
+                     return Container(
+
+                     );
+
+                   }
+               ),
+             );
+           }
+        }
+      ),
 
     );
   }
