f42bae8696520d195b71dff1858447422d7f7fde
Yazdani <yaz4dani@gmail.com>
2020-03-03 14:24:11 +0600

    Implement Firestore

--- lib/london/See.dart
+++ lib/london/See.dart
@@ -8,50 +8,84 @@ class See extends StatefulWidget {
 
 class _SeeState extends State<See> {
 
-  Future getPlaces()async {
+  Future getPlaces() async {
     var firestore = Firestore.instance;
-    QuerySnapshot snapshot = await firestore.collection("seePlaces").getDocuments();
+    QuerySnapshot snapshot = await firestore.collection("seePlaces")
+        .getDocuments();
     return snapshot.documents;
   }
-  Future getRefresh()async{
+
+  Future getRefresh() async {
     await Future.delayed(Duration(seconds: 3));
     setState(() {
       getPlaces();
     });
   }
-
+  final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+  final double itemWidth = size.width / 2;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      
+
       appBar: AppBar(
         title: Text("Tourist Places"),
         backgroundColor: Colors.amber,
       ),
 
       body: FutureBuilder(
-         future: getPlaces(),
-        builder: (BuildContext context,snapshot){
-           if(snapshot.connectionState == ConnectionState.waiting){
-             return Center(
-               child: CircularProgressIndicator(),
-             );
-           }else{
-             return RefreshIndicator(
-               onRefresh: getRefresh,
-               child: GridView.builder(
-                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                   itemBuilder: (BuildContext c,index){
-                     return Container(
+          future: getPlaces(),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return RefreshIndicator(
+                onRefresh: getRefresh,
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                    itemCount: snapshot.data.length,
+                    childAspectRatio: (itemWidth / itemHeight),
+                    itemBuilder: (BuildContext c, index) {
+                      var ourData = snapshot.data[index];
+                      return Container(
+                        child: Card(
+                          elevation: 10.0,
+                          child: Column(
+                            children: <Widget>[
 
-                     );
+                              Container(
+                                child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Image.network(ourData.data['img'],
+                                    height: 100.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                ),
+                              ),
+                              SizedBox(height: 5,),
+                              Container(
+                                padding: EdgeInsets.all(6.0),
+                                child: Text(ourData.data['name'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                  fontSize: 20.0
+                                ),
+                                ),
+                              )
 
-                   }
-               ),
-             );
-           }
-        }
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                ),
+              );
+            }
+          }
       ),
 
     );
