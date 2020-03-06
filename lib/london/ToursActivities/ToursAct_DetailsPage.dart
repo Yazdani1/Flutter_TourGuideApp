eb9409223f1222d1db39c6f0c62ae5f35b7c5da3
Yazdani <yaz4dani@gmail.com>
2020-03-06 20:57:16 +0600

    Implement Firestore

--- lib/london/ToursActivities/ToursAct_DetailsPage.dart
+++ lib/london/ToursActivities/ToursAct_DetailsPage.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
-
-
 class ToursActivitiesDetails extends StatefulWidget {
   DocumentSnapshot snapshot;
   ToursActivitiesDetails(this.snapshot);
